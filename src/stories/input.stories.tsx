79c8b40ffca3f78b735e25dc4ba2362795969497
DinaLysenko <dinafate@rambler.ru>
2024-01-28 22:59:10 +0500

    add input stories

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -18,13 +18,39 @@ export const GetValueOfUncontrolledInput = () => {
     const [value, setValue] = useState('')
     const inputValue = useRef<HTMLInputElement | null>(null)
     const onClickHandler = () => {
-        if(inputValue.current){
+        if (inputValue.current) {
             setValue(inputValue.current.value)
         }
     }
     return <><input ref={inputValue}/>
         <button onClick={onClickHandler}>save</button>
         -{value}</>
+}
+export const ControlledInput = () => {
+    const [value, setValue] = useState('')
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value)
+    }
+    return <input value={value} onChange={onChangeHandler}/>
+}
+export const ControlledCheckbox = () => {
+    const [checkedValue, setCheckedValue] = useState(true)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setCheckedValue(e.currentTarget.checked)
+    }
+    return <input type={'checkbox'} checked={checkedValue} onChange={onChangeHandler}/>
+}
+export const ControlledSelect = () => {
+const[valueSelect, setValueSelect]=useState<string|undefined>('2')
+    const onChangeHandler=(e:ChangeEvent<HTMLSelectElement>)=>{
+    setValueSelect(e.currentTarget.value)
+    }
+    return <select value={valueSelect} onChange={onChangeHandler}>
+        <option >none</option>
+        <option value="1">Moscow</option>
+        <option value="2">Minsk</option>
+        <option value="3">Ufa</option>
+    </select>
 }
 export const ControlledInputWthFixedValue = () => {
     return <input value={'it-incubator'}/>

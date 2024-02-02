354160505d56e372747a312d30d33919522e5c7e
DinaLysenko <dinafate@rambler.ru>
2024-02-02 11:58:10 +0500

    add Select.tsx

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, KeyboardEvent, useEffect} from 'react';
 import s from './Select.module.css'
 
 type ItemsType = {
@@ -6,34 +6,58 @@ type ItemsType = {
     title: string
 }
 type SelectType = {
-    value: any
+    value?: any
     onChange: (value: any) => void
     items: ItemsType[]
 }
 export const Select = (props: SelectType) => {
-    const [selectValue, setSelectValue] = useState(props.value)
     const [selectFocus, setSelectFocus] = useState(false)
-    const onBlurHandler=()=>{
-        setSelectFocus(selectFocus)
-        setSelectValue(selectValue)
-    }
-    const onClickHandler = () => {
+    const [hoveredItemValue, setHoveredItemValue] = useState(props.value)
+    const selectedItem = props.items.find(i => i.id === props.value)
+    const hoveredItem = props.items.find(i => i.id === hoveredItemValue)
+    useEffect(() => {
+        setHoveredItemValue(props.value)
+    }, [props.value])
+    const toggleItems = () => {
         setSelectFocus(!selectFocus)
     }
-    const onClickItem =(id: number)=>{
-       const item= props.items.filter(el=>el.id ===id)
-        setSelectValue(item[0].title)
-        setSelectFocus(!selectFocus)
+    const onClickItem = (id: any) => {
+        props.onChange(id)
+        toggleItems()
+    }
+    const onKeyUp = (e: KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'ArrowDown' || e.key === 'ArrowUp') {
+            for (let i = 0; i < props.items.length; i++) {
+                if (hoveredItemValue === props.items[i].id) {
+                    const pretendentElement = e.key === 'ArrowDown' ? props.items[i + 1] : props.items[i - 1]
+                    if (pretendentElement)
+                        props.onChange(pretendentElement.id)
+                    return
+                }
+            }
+        }
+        if (!selectedItem) {
+            props.onChange(props.items[0].id)
+        }
+        if (e.key === 'Enter' || e.key === 'Escape') {
+            setSelectFocus(false)
+        }
+
     }
     return (
         <div>
-            <div className={selectFocus? s.selectFocus : s.select} onClick={onClickHandler} onBlur={onBlurHandler} ><span className={s.selectValue}>{selectValue}</span>
-                <button className={s.button}>{!selectFocus? 'V' : "^"}</button>
+            <div className={s.select + ' ' + (selectFocus ? s.selectFocus : '')} onClick={toggleItems} onKeyUp={onKeyUp}
+                 tabIndex={0}>
+                <span className={s.selectValue}>{selectedItem && selectedItem.title}</span>
+                <button className={s.button}>{!selectFocus ? 'V' : "^"}</button>
             </div>
             {selectFocus && <div className={s.list}> {props.items.map(i => {
-                return <div className={s.listItem} onClick={()=>onClickItem(i.id)} key={i.id}>{i.title}</div>
+                return <div
+                    onMouseEnter={() => setHoveredItemValue(i.id)}
+                    className={s.listItem + ' ' + (hoveredItem === i ? s.selected : '')}
+                    onClick={() => onClickItem(i.id)} key={i.id}>{i.title}</div>
             })}
-        </div>}
+            </div>}
         </div>
     );
 };

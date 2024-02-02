420af5b0392b2f2e727d2cf3343ef61dbbfb314c
DinaLysenko <dinafate@rambler.ru>
2024-02-02 11:56:32 +0500

    add Select.stories.tsx

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,5 +1,6 @@
 import {action} from "@storybook/addon-actions";
 import {Select} from "./Select";
+import {useState} from "react";
 
 
 export default {
@@ -9,5 +10,10 @@ export default {
 const onChangeHandler = action('onClick')
 
 export const SelectWithValue = () => {
-    return <Select items={[ {id: 0, title: 'none'},{id: 1, title: 'Moscow'}, {id: 2, title: 'Minsk'}, {id: 3, title: 'Ufa'}]} value={2} onChange={onChangeHandler}/>
+    const [value, setValue] = useState('2')
+    return <Select items={[{id: '1', title: 'Moscow'}, {id: '2', title: 'Minsk'}, {id: '3', title: 'Ufa'}]} value={value} onChange={setValue}/>
+}
+export const SelectWithoutValue = () => {
+    const [value, setValue] = useState(null)
+    return <Select items={[{id: '1', title: 'Moscow'}, {id: '2', title: 'Minsk'}, {id: '3', title: 'Ufa'}]}  value={value} onChange={setValue}/>
 }

9ee50021a7b34ea6d48978d9f6e733d84b00c47c
DinaLysenko <dinafate@rambler.ru>
2024-02-10 12:04:41 +0500

    add setInterval

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -26,3 +26,34 @@ export const SimpleExample=()=>{
         <button onClick={()=>setCount(count+1)}>count +</button>
     </div>
 }
+export const SimpleExampleSetTimeout=()=>{
+    console.log('SimpleExampleSetTimeout')
+    const[fake, setFake]=useState(1)
+    const[count, setCount]=useState(1)
+    useEffect(()=>{
+        setInterval(()=>{
+           setCount((state)=>state+1)
+        }, 1000)
+    },[])
+    return <div>
+        Count-{count} ---Fake-{fake}
+        {/*<button onClick={()=>setFake(fake+1)}>fake +</button>*/}
+        {/*<button onClick={()=>setCount(count+1)}>count +</button>*/}
+    </div>
+}
+export const SimpleExampleClock=()=>{
+    let[seconds, setSeconds]=useState(0)
+    let data=new Date()
+   seconds=data.getSeconds()
+    let minutes=data.getMinutes()
+    let hours=data.getHours()
+    useEffect(()=>{
+        setInterval(()=>{
+           setSeconds(state=>state+1)
+        }, 1000)
+    },[])
+
+    return <div>
+        {hours}:{minutes<10? '0' + minutes:minutes}:{seconds<10? '0' + seconds: seconds}
+    </div>
+}
